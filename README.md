52e4c74feea4cb8283cbd1e0b3b669097f50ba38
Do Bryan <110252@eesd.org>
2021-01-15 00:16:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Concatenation 
+
+`+` Adding strings together, appending (add to the end)
+
+"Hello" + "World" --> Hello World
+
+## Type Cast
+Forcing a data type on a variable
+
+### To force an interger into a strings
+`str(variablename)`
+
+### To force a string into a interger
+`int(variableName)`
+
+## Input
+
+Input is the way the computer gets information from the user
+
+Python initally treats input as __string__ type 
+
+Ex.
+5 --> "5"
+Today is Thursday --> "Today is Thursday"
+
+### Formula:
+`variableName = input("Optional Prompt")`
+Python stores the __input__ in the __variableName__
---
+++ main.py
@@ -0,0 +1,27 @@
+print("Hello " + "World" + " Today is Thursday")
+# This is concatenation of strings
+print("This starts a new line")
+print()
+print("Tomorrow is Friday")
+
+# If adding strings is called "concatenation"
+# What is + for intergers called? additon
+# 5 + 4 = 9 
+
+print ("5" + "4") #Concatenation
+print (5 + 4) # Addition
+
+firstName = "I don't have a name"
+print("My name is " + firstName)
+age = 1
+
+print("My age is " + str(age))
+
+# firstname != FirstName 
+
+userName = input("What is your name? ")
+print("Hey " + userName)
+
+# Ask the user for their birth year
+birthYear = input("What year were your born in? ")
+print(2021-int(birthYear))
